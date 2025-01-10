3527ec2161f32fb1ab5ac4f752484f01206d9581
LazarNedelja <lazalaki069@gmail.com>
2025-01-10 16:09:34 +0100

    DB populated

--- entities/src/main/java/com/cloudify/entities/LoyaltyEnrollment.java
+++ entities/src/main/java/com/cloudify/entities/LoyaltyEnrollment.java
@@ -6,8 +6,56 @@ import java.io.Serializable;
 @Schema(description = "Loyalty enrollment details")
 public class LoyaltyEnrollment implements Serializable {
 
-    @Schema(description = "User ID", required = true, example = "101")
-    private Integer userId;
+    public enum Tier {
+        Silver, Gold, Platinum
+    }
 
-    // Getters, Setters, Constructors
+    @Schema(description = "User", required = true, implementation = User.class)
+    private User user;
+
+    @Schema(description = "Loyalty points balance", example = "100")
+    private Integer pointsBalance;
+
+    @Schema(description = "Loyalty tier", example = "Gold")
+    private Tier tier;
+
+    // Default constructor
+    public LoyaltyEnrollment(User user, Integer pointsBalance, Tier tier) {
+        this.user = user;
+        this.pointsBalance = pointsBalance;
+        this.tier = tier;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Integer getPointsBalance() {
+        return pointsBalance;
+    }
+
+    public void setPointsBalance(Integer pointsBalance) {
+        this.pointsBalance = pointsBalance;
+    }
+
+    public Tier getTier() {
+        return tier;
+    }
+
+    public void setTier(Tier tier) {
+        this.tier = tier;
+    }
+
+    @Override
+    public String toString() {
+        return "LoyaltyEnrollment{" +
+                "user=" + user +
+                ", pointsBalance=" + pointsBalance +
+                ", tier=" + tier +
+                '}';
+    }
 }
